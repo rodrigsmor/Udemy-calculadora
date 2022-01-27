b4ed71cb8450b8c8a4ed988c3f564956d59833e0
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-01-27 11:19:50 -0300

    Aula 9 e 10 - Adiciona e trata as operações

--- script/controller/CalcController.js
+++ script/controller/CalcController.js
@@ -1,5 +1,6 @@
 class CalcController {
     constructor() {
+        this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
@@ -7,6 +8,8 @@ class CalcController {
         this._currentDate;
         this.initialize();
         this.initButtonsEvent();
+        this.execBtn();
+        this.addOperation();
     }
 
     initialize() {
@@ -23,12 +26,113 @@ class CalcController {
         });
     }
 
+    clearAll() {
+        this._operation = [];
+    }
+
+    clearEntry() {
+        this._operation.pop();
+    }
+
+    getLastOperation() {
+        return this._operation[this._operation.length - 1];
+    }
+
+    setLastOperation(value) {
+        this._operation[this._operation.length - 1] = value;
+    }
+
+    isOperator(value) {
+        return ['+', '-', '*', '%', '/'].indexOf(value) > -1;
+    }
+
+    addOperation(value) {
+        if(isNaN(this.getLastOperation())) {
+            if(this.isOperator(value)) {
+                this.setLastOperation(value);
+            } else if(isNaN(value)) {
+                console.log(value);
+            } else {
+                this._operation.push(value);
+            }
+        } else {
+            let newValue = this.getLastOperation().toString() + value.toString();
+
+            this.setLastOperation(parseInt(newValue));
+        }
+
+        console.log(this._operation);
+    }
+
+    setError() {
+        this.displayCalc = 'Error';
+    }
+
+    execBtn(value) {
+        switch(value) {
+            case 'ac':
+                this.clearAll();
+            break;
+
+            case 'ce':
+                this.clearEntry();
+            break;
+
+            case 'soma':
+                this.addOperation('+');
+            break;
+
+            case 'subtracao':
+                this.addOperation('-');
+            break;
+
+            case 'divisao':
+                this.addOperation('/');
+            break;
+
+            case 'multiplicacao':
+                this.addOperation('*');
+            break;
+
+            case 'porcento':
+                this.addOperation('%');
+            break;
+
+            case 'igual':
+                this.clearAll();
+            break;
+
+            case 'ponto':
+                this.addOperation('.');
+            break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+            break;
+
+            default:
+                this.setError();
+            break;
+        }
+    }
+
     initButtonsEvent() {
         let buttons = document.querySelectorAll('#buttons > g, #parts > g');
 
         buttons.forEach((btn, index) => {
             this.addEventListenerAll(btn, 'click drag', event => {
-                console.log(btn.className.baseVal.replace("btn-", ""));
+                let textButton = btn.className.baseVal.replace("btn-", "");
+
+                this.execBtn(textButton);
             });
 
             this.addEventListenerAll(btn, 'mouseover mouseup mousedown', event => {
