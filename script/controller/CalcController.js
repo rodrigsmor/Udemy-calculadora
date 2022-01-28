2e04d2b0cdc73d47195b9ec97c16e9b39114176f
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-01-28 12:19:54 -0300

    AUla 21 - Trata erros no Eval

--- script/controller/CalcController.js
+++ script/controller/CalcController.js
@@ -155,7 +155,13 @@ class CalcController {
     }
     
     getResult() {
-        return eval(this._operation.join(""));
+        try {
+            return eval(this._operation.join(""));
+        } catch (error) {
+            setTimeout(() => {
+                this.setError();
+            }, 10);
+        }
     }
 
     calc() {
