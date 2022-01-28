930fb6bb527a4a37b524110e60068ebda156658e
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-01-28 11:02:04 -0300

    Aula 15 - Adiciona ponto a calculadora

--- script/controller/CalcController.js
+++ script/controller/CalcController.js
@@ -30,6 +30,8 @@ class CalcController {
 
     clearAll() {
         this._operation = [];
+        this._lastNumber = '';
+        this._lastOperator = '';
         this.setLastNumberToDisplay();
     }
 
@@ -126,8 +128,6 @@ class CalcController {
         if(isNaN(this.getLastOperation())) {
             if(this.isOperator(value)) {
                 this.setLastOperation(value);
-            } else if(isNaN(value)) {
-                console.log(value);
             } else {
                 this.pushOperation(value);
                 this.setLastNumberToDisplay();
@@ -137,7 +137,7 @@ class CalcController {
                 this.pushOperation(value);
             } else {
                 let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(parseInt(newValue));
+                this.setLastOperation(parseFloat(newValue));
 
                 this.setLastNumberToDisplay();
             }
@@ -146,6 +146,18 @@ class CalcController {
 
     setError() {
         this.displayCalc = 'Error';
+    }
+
+    addDot() {
+        let lastOperation = this.getLastOperation();
+    
+        if(this.isOperator(lastOperation) || !lastOperation) {
+            this.pushOperation('0.');
+        } else {
+            this.setLastOperation(lastOperation.toString() + '.');
+        }
+
+        this.setLastNumberToDisplay();
     }
 
     execBtn(value) {
@@ -183,7 +195,7 @@ class CalcController {
             break;
 
             case 'ponto':
-                this.addOperation('.');
+                this.addDot('.');
             break;
 
             case '0':
@@ -196,7 +208,7 @@ class CalcController {
             case '7':
             case '8':
             case '9':
-                this.addOperation(parseInt(value));
+                this.addOperation(parseFloat(value));
             break;
 
             default:
