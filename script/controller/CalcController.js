51312644abdf5bc386cd4cc5abdc4e9fb99d68b0
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-01-28 11:53:39 -0300

    Aula 19 - Adiciona áudio a calculadora

--- script/controller/CalcController.js
+++ script/controller/CalcController.js
@@ -1,5 +1,7 @@
 class CalcController {
     constructor() {
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = '';
         this._operation = [];
@@ -44,10 +46,29 @@ class CalcController {
 
         this.setLastNumberToDisplay();
         this.pasteFromClipboard();
+
+        document.querySelectorAll('.btn-ac').forEach(btn => {
+            btn.addEventListener('dblclick', event => {
+                this.toggleAudio();
+            });
+        });
+    }
+
+    toggleAudio() {
+        this._audioOnOff = !this._audioOnOff;
+    }
+
+    playAudio() {
+        if(this._audioOnOff) {
+            this._audio.currentTime = 0;
+            this._audio.play()
+        }
     }
 
     initKeyboard() {
         document.addEventListener('keyup', event => {
+            this.playAudio();
+            
             switch(event.key) {
                 case 'Escape':
                     this.clearAll();
@@ -236,6 +257,8 @@ class CalcController {
     }
 
     execBtn(value) {
+        this.playAudio();
+
         switch(value) {
             case 'ac':
                 this.clearAll();
